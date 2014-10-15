4b5722714eb2a61e16112dc48922f9074ce028c4
Andrew Koenig <axk122530@utdallas.edu>
2014-10-15 23:11:46 +0000

    Added usage details

--- freq.py
+++ freq.py
@@ -3,13 +3,17 @@
 #
 # Quick script to change the frequency of the DORJI module
 #
+# Usage: freq.py 144.3900 144.3900 0015 /dev/ttyUSBO
+# 		 TX freq  RX freq  PL Tone  /dev/
+# PL Tone 110.9 = 0015
+#
+
 
 import serial
 import sys
 
-
 arg = sys.argv 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial(arg[4], 9600, timeout=1)
 
 ser.write('AT+DMOCONNECT\r\n')
 print ser.readline()
